19b74df8c78287fa4d785844946b728ae70334a7
Arkadiusz Holko <fastred@fastred.org>
2015-12-18 11:13:42 +0100

    Add en empty project

---
+++ CIExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIExample
+//
+//  Created by Arkadiusz Holko on 18-12-15.
+//  Copyright © 2015 Macoscope sp. z o.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
