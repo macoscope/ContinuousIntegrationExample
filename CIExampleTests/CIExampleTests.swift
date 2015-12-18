3ef40ec5a7fd812eccbc1d8c93ab87c063b00250
Arkadiusz Holko <fastred@fastred.org>
2015-12-18 12:44:32 +0100

    Add some dummy test

--- CIExampleTests/CIExampleTests.swift
+++ CIExampleTests/CIExampleTests.swift
@@ -10,27 +10,9 @@ import XCTest
 @testable import CIExample
 
 class CIExampleTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
+
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let a = 1
+        XCTAssert(a + 1 == 2)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
