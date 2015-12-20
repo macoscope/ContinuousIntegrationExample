4218009871185f6dc5782fef53ee0b7296f0a4b9
Arkadiusz Holko <fastred@fastred.org>
2015-12-20 19:11:37 +0100

    Run "fastlane beta" on push to develop

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -5,3 +5,13 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   exit $?
 fi
 
+if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+  # Travis fetches a shallow clone. We use commit count until HEAD for build number. In order to assure that the count is correct we have to `unshallow` Travis' clone.
+  git fetch --unshallow
+
+  # We need this because otherwise there are issues with SSL in Ruby in xcode7.2 OS X image
+  rvm reinstall 2.0.0-p643 --disable-binary
+  fastlane beta
+  exit $?
+fi
+
